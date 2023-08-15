6abbc1c3c2c85e11a67cc858fb663e1d5b5b317d
Marcos Rollón <marcosrollonrivas@gmail.com>
2023-08-15 15:25:08 +0200

    Improved testing ui

--- public/light_tester.js
+++ public/light_tester.js
@@ -1,6 +1,10 @@
 const socket = io("http://localhost:3000");
 const lightContainer = document.getElementById("light-container");
-
+const topLeft = document.getElementById("top-left");
+const topRight = document.getElementById("top-right");
+const botLeft = document.getElementById("bot-left");
+const botRight = document.getElementById("bot-right");
+const ball = document.getElementById("center-ball");
 
 document.body.onload = main;
 let decays = [];
@@ -15,26 +19,72 @@ socket.on("setWithDecay", (data) => {
 });
 
 function main() {
-    for (let i = 0; i < 21; i++) {
-        let light = document.createElement("div");
-        light.classList.add("light");
-        let indicator = document.createElement("p");
-        indicator.innerHTML = i;
-        let div = document.createElement("div");
-        div.classList.add("flex");
-        div.appendChild(light);
-        div.appendChild(indicator);
-        lightContainer.appendChild(div);
-
+    addLedsToCuadrante(topLeft, false, true, 0);
+    addLedsToCuadrante(topRight, true, true, 5);
+    addLedsToCuadrante(botLeft, false, false, 10);
+    addLedsToCuadrante(botRight, true, false, 15);
+    for (let i = 0; i < 20; i++) {
         decays.push(null);
     }
 }
 
+function addLedsToCuadrante(cuadrante, xPositiva, yPositiva, startValue) {
+    for (let i = 0; i < 5; i++) {
+        let light = document.createElement("div");
+        light.classList.add("light");
+        let indicator = document.createElement("p");
+        indicator.innerHTML = startValue + i;
+        let div = document.createElement("div");
+        div.style.position = "absolute";
+        div.style.display = "flex";
+        div.style.flexDirection = "column";
+        div.appendChild(light);
+        div.appendChild(indicator);
+
+        const CONSTANTE_VERTICAL = 50
+        const CONSTANTE_HORIZONTAL = 70;
+        const padding = 5;
+
+        if (xPositiva) {
+            if (yPositiva) {
+                div.style.top = `${i * CONSTANTE_VERTICAL + padding}px`;
+                div.style.left = `${i * CONSTANTE_HORIZONTAL + padding}px`;
+            }
+            else {
+                div.style.top = `${(400 - 200) - i * CONSTANTE_VERTICAL + padding}px`;
+                div.style.left = `${i * CONSTANTE_HORIZONTAL + padding}px`;
+            }
+        }
+        else {
+            if (yPositiva) {
+                div.style.top = `${i * CONSTANTE_VERTICAL + padding}px`;
+                div.style.left = `${(600 - 20) - i * CONSTANTE_HORIZONTAL - padding}px`;
+            }
+            else {
+                div.style.top = `${(400 - 200) - i * CONSTANTE_VERTICAL + padding}px`;
+                div.style.left = `${(600 - 20) - i * CONSTANTE_HORIZONTAL - padding}px`;
+            }
+        }
+
+        cuadrante.appendChild(div);
+    }
+}
+
+
 function setLightOpacity(number, opacity) {
-    // get correct child
-    let container = lightContainer.children[number];
-    let light = container.children[0];
-    light.style.backgroundColor = `rgba(0,255,0,${opacity})`;
+    let light;
+    if (number < 5) {
+        light = topLeft.children[number + 1].children[0];
+    } else if (number < 10) {
+        light = topRight.children[number + 1 - 5].children[0];
+    } else if (number < 15) {
+        light = botLeft.children[number + 1 - 10].children[0];
+    } else if (number < 20) {
+        light = botRight.children[number + 1 - 15].children[0];
+    } else {
+        light = ball;
+    }
+    light.style.backgroundColor = `rgba(0, 255, 0, ${opacity})`;
 }
 
 
